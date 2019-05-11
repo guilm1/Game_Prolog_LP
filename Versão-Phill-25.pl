cc31e2116c5b61c50ee6b187d617518dc1615a19
Phill-25 <35849468+Phill-25@users.noreply.github.com>
2019-05-11 11:47:33 -0300

    Update Versão-Phill-25.pl
    Possível resolução...

--- Versão-Phill-25.pl
+++ Versão-Phill-25.pl
@@ -1,1 +1,9 @@
-teste commit
+//Ideias:
+/* 
+seek: função de busca por indice e retorna a linha e a uma coluna antes, retorna a calda. O fato de retornar uma coluna antes é para na proxima função manipular.
+Essa função chama a função game.
+
+game: pega a cabeça da lista, testa para ver se é > que 0 se for(->;) chama a seek com as coordenadas.
+Possui outras chamadas com coordenadas que simulam cima, baixo, direita e esquerda.
+
+*/
