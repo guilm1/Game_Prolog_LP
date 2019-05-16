23dce9e3b65c180f49548da7f82f604b11cb4abf
Phill-25 <35849468+Phill-25@users.noreply.github.com>
2019-05-16 19:03:39 -0300

    Update Versão-Phill-25.pl
    Busca pelo Indice

--- Versão-Phill-25.pl
+++ Versão-Phill-25.pl
@@ -1,4 +1,3 @@
-//Ideias:
 /* 
 seek: função de busca por indice e retorna a linha e a uma coluna antes, retorna a calda. O fato de retornar uma coluna antes é para na proxima função manipular.
 Essa função chama a função game.
@@ -7,3 +6,28 @@ game: pega a cabeça da lista, testa para ver se é > que 0 se for(->;) chama a 
 Possui outras chamadas com coordenadas que simulam cima, baixo, direita e esquerda.
 
 */
+
+%-----------Busca pelo Indice--------------------------
+
+busca(0,[X|_L], X).
+busca(I, [X|LS], R):-
+	I1 is I-1,
+	busca(I1, LS, R).
+
+finder(I, J, M, R):-
+	busca(J, M, L) ->
+	busca(I, L, E), R is E;
+	fail.
+
+%--------------------------------------------------------
+
+
+busca1(0,[X|_L], X).
+busca1(I, [X|LS], R):-
+	I1 is I-1,
+	busca1(I1, LS, R).
+
+finder1(I, J, M, R):-
+	busca1(J, M, L) ->
+	busca1(I, L, E), R is E;
+	fail.
