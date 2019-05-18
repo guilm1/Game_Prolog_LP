84ace46ce9b4873815117e1bf067a24e19b686b3
Phill-25 <35849468+Phill-25@users.noreply.github.com>
2019-05-18 17:19:37 -0300

    Test Game
    Recebe uma matriz qualquer e decrementa uma unidade em todas as suas posições.

--- Versão-Phill-25.pl
+++ Versão-Phill-25.pl
@@ -7,16 +7,54 @@ Possui outras chamadas com coordenadas que simulam cima, baixo, direita e esquer
 
 */
 
+%-------------------insere no inicio------------------------------------------
+insereInicio(X,[], [X]).
+insereInicio(X, XS, [X|XS]):- !.
+
+
+%-------------------insere no fim------------------------------------------
+insereFim(X, [], [X]).
+
+insereFim(X, [Y], L):-
+	insereInicio(Y, [X], L).
+
+insereFim(X, [Y|YS], L):-
+	insereFim(X, YS, ZS),
+	insereInicio(Y, ZS, L).
 %-----------Busca pelo Indice--------------------------
 
-busca(0,[X|_L], X).
+busca(0,[X|LS], X).
+
 busca(I, [_X|LS], R):-
 	I1 is I-1,
 	busca(I1, LS, R).
-
+	
+	
 finder(I, J, M, R):-
 	busca(I, M, L) ->
 	busca(J, L, E), R is E;
 	fail.
+%----------------------Teste Game----------------------------------
 
-%--------------------------------------------------------
+ateFim([],[]).
+ateFim([E|Li], RLi):- % Pega todos os elementos da linha, decrementa, e coloca em uma nova lista
+	
+	ateFim(Li, NLi),
+	EN is E-1,
+	insereInicio(EN,NLi, RLi).
+	
+
+testGame([],_Rmtz):-!.
+testGame([Li|Mtz], RMtz):- % pega uma "linha" da matriz
+	testGame(Mtz, IMtz),
+	ateFim(Li, DLi),
+	insereFim(DLi, IMtz, RMtz).
+	
+
+
+
+
+
+
+
+
