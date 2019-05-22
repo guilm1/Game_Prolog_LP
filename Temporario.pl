b77c4eefc9ce1d009872b3ede151a1b9eaae7ce2
Guilherme <guilherme.argomes@gmail.com>
2019-05-22 04:34:09 -0300

    new Commit w impression

--- Temporario.pl
+++ Temporario.pl
@@ -7,51 +7,14 @@ Possui outras chamadas com coordenadas que simulam cima, baixo, direita e esquer
 
 */
 %------------------Tamanho da Lista--------------------------------------
-
 tamLista([_],0).  % retornar quantidade de linhas ou Colunas
 tamLista([_|XS],R):- tamLista(XS, A), R is A+1.
 
-%-------------------insere no inicio------------------------------------------
-insereInicio(X,[], [X]).
-insereInicio(X, XS, [X|XS]):- !.
-
-
-%-------------------insere no fim------------------------------------------
-insereFim(X, [], [X]).
-
-insereFim(X, [Y], L):-
-	insereInicio(Y, [X], L).
-
-insereFim(X, [Y|YS], L):-
-	insereFim(X, YS, ZS),
-	insereInicio(Y, ZS, L).
 %-----------Busca pelo Indice--------------------------
-
 busca(0,[X|_LS], X).
+busca(I, [_X|LS], R):- I1 is I-1, busca(I1, LS, R).
 
-busca(I, [_X|LS], R):-
-	I1 is I-1,
-	busca(I1, LS, R).
-
-  espaco :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
-            write(F,'\n'), write('\n'),
-            close(F).
-
-  printMatriz(-1, _, _) :- !.     % Predicado para printar matriz
-  printMatriz(L, C, M) :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
-                          write(F, '|'), write('|'),
-                          busca(L, M, R1), write(F, R1), write(F, '|\n'),
-                          write(R1), write('|\n'),close(F),
-                          L1 is L - 1,
-                          printMatriz(L1,C,M), espaco.
-
-
-finder(I, J, M, R):-
-	busca(I, M, L) ->
-	busca(J, L, E), R is E;
-	fail.
-
-%---------------------------------------------------------------------------
+finder(I, J, M, R):- busca(I, M, L) -> busca(J, L, E), R is E; fail.
 
 % -------------------------------------------------------------------
 % Passar o elemento da lista que eu quero trocar, a lista e o elemento novo
@@ -66,125 +29,167 @@ remover(X,L1,L).	%  A inserção é sempre feita na cabeça de L.
 
 swap(E, ET, [E|XS], R) :- inserir(ET,XS,R).
 swap(E, ET, [X|XS], [X|R]) :- swap(E,ET,XS,R).
+
 %-----------------------------------------------------
+% insere a nova lista com o elemento decrementado na posição certa
+insereEspecifico(0, R2, [_|M], R) :- inserir(R2, M, R).
 
+% Enquanto indice não chegar a zero, serão adicionadas as listas dentro da lista
+insereEspecifico(I, R2, [X|M], [X|R]) :- I1 is I -1, insereEspecifico(I1,R2, M, R).
 
-insereEspecifico(0, R2, [_|M], R) :- inserir(R2, M, R). % insere a nova lista com o elemento decrementado na posição certa
+% Irá retornar a lista específica com o elemento decrementado
+buscaTroca(0, [X|LS], R) :- B is X - 1, inserir(B,LS,R).
 
-insereEspecifico(I, R2, [X|M], [X|R]) :- I1 is I -1,    % Enquanto indice não chegar a zero, serão adicionadas as listas dentro da lista
-                                         insereEspecifico(I1,R2, M, R).
+% Enquanto o índice nao chegar a zero
+buscaTroca(I, [X|LS], [X|R]) :- 	I1 is I-1, buscaTroca(I1,LS,R).
 
-buscaTroca(0, [X|LS], R) :- B is X - 1,       % Irá retornar a lista específica com o elemento decrementado
-                            inserir(B,LS,R).
-
-buscaTroca(I, [X|LS], [X|R]) :- 	I1 is I-1,   % Enquanto o índice nao chegar a zero
-                                  buscaTroca(I1,LS,R).
-
-
-
+% Decrementa Posição (I,J) na matriz
 troca(L, C, M, R) :- busca(L, M, R1) ->
                      buscaTroca(C, R1, R2),
                      insereEspecifico(L, R2, M, R).
-%----------------------Teste Game----------------------------------
 
-ateFim([],[]).
-ateFim([E|Li], RLi):- % Pega todos os elementos da linha, decrementa, e coloca em uma nova lista
+%-------------------------------------Impressões--------------------------------------------
+marcadorAux(0, [_|LS], R) :- inserir('P*',LS,R).
+marcadorAux(I, [X|LS], [X|R]) :- 	I1 is I-1, marcadorAux(I1,LS,R). % mesmaLogica pred buscaTrocaa
 
-	ateFim(Li, NLi),
-	EN is E-1,
-	insereInicio(EN,NLi, RLi).
+colocaListaMarcada(0, R2, [_|M], R) :- inserir(R2, M, R).
+colocaListaMarcada(I, R2, [X|M], [X|R]) :- I1 is I -1, colocaListaMarcada(I1,R2, M, R).
 
+trocaPorMarcador(L, C, M, R) :- busca(L, M, R1) -> marcadorAux(C, R1, R2), colocaListaMarcada(L, R2, M, R). % mesma Logica pred trocaa
 
-testGame([],_Rmtz):-!.
-testGame([Li|Mtz], RMtz):- % pega uma "linha" da matriz
-	testGame(Mtz, IMtz),
-	ateFim(Li, DLi),
-	insereFim(DLi, IMtz, RMtz).
+separador :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+             write(F,'\n__________________________________\n'), write('\n________________________________\n'),
+             close(F).
+
+espaco :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+          write(F,'\n'), write('\n'),
+          close(F).
+
+% Predicado para imprimir matriz
+printMatriz(-1, _, _, _, _) :- !.
+printMatriz(L, C, I, J, M) :- trocaPorMarcador(I,J,M,R),
+													    open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+                              write(F, '|'), write('|'), busca(L, R, R1), write(F, R1), write(F, '|\n'),
+                              write(R1), write('|\n'),close(F), L1 is L - 1, printMatriz(L1,C,I,J,R).
+
+% imprime coordenadas da posição atual
+mostraPosicao(I,J) :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+										  write(F,'Posição Atual: ('),write(F, I),write(F,', '),write(F,J),write(F,')\n'), close(F),
+											write('Posicao Atual: ('),write(I),write(', '),write(J),write(')\n').
+
+% imprime movimento feito, a matriz resultante e o valor do marcador P*
+mostraDirecao(I, J, QL, QC, Mtz, M) :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+	                                     write(F,'Movimento: '), write(F,M), write(F,'\nPosição Atual: ('),write(F, I),write(F,', '),
+																			 write(F,J),write(F,')\n'), close(F), write('Movimento: '), write(M),write('\nPosicao Atual: ('),
+																			 write(I),write(', '),write(J),write(')\n'),mostraValor(I,J,Mtz), printMatriz(QL,QC,I,J,Mtz), separador.
+
+% imprime valor do marcador P*
+mostraValor(I,J,M) :- open('c:/users/adriana/desktop/disciplinas19.1/linguagens/game_prolog_lp/matriz.txt',append,F),
+											finder(I,J,M,E), write(F,'Valor P*= '),write(F,E),write('Valor P*: '),write(E),write(F,'\n'),
+											write('\n'), close(F).
 
 %-----------------Verifica Movimento------------------------------------
+% recebe coordenadas (i,j) e uma matriz. Em seguida, busca-se o elemento das
+% coordenadas e Testa se o elemento é diferente de -1.
+checkingMove(I, J, Mtz):- finder(I, J, Mtz, F),  F \= -1.
 
-checkingMove(I, J, Mtz):- % recebe coordenadas (i,j) e uma matriz.
+% Confere se os índices passados não estão fora do limite e executa o predicado acima.
+checkingMaster(I, J, Mtz):- tamLista(Mtz, TI), busca(0, Mtz, HM), tamLista(HM, TJ),
+														I =< TI, I > -1, J =< TJ, J > -1, checkingMove(I, J, Mtz);
+														false.
 
-	finder(I, J, Mtz, F),    % em seguida, busca-se o elemento das coordenadas
-	F \= -1.		 			% Testa se o elemento é diferente de -1.
-
-
-%-------------------------Testes---------------------------------------------------
-
-
-checkingMaster(I, J, Mtz, M):-
-	tamLista(Mtz, TI),
-	busca(0, Mtz, HM),
-	tamLista(HM, TJ),
-	I =< TI, I > -1,
-	J =< TJ, J > -1,
-	checkingMove(I, J, Mtz) ->
-	write(M), write('\n');
-	false.
 %-------------Movimento------------------------------------------------------------------
 moveE(I, J, Mtz, X):-
 	%Esquerda
 	E is J-1,
-	checkingMaster(I, E, Mtz, esquerda) -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveE(I, E, R, X);
+	checkingMaster(I, E, Mtz) -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(I,E,QLinha, QColuna,R, 'Esquerda'),
+	moveE(I, E, R, X);
 	%Cima
 	C is I+1,
-	checkingMaster(C, J, Mtz, cima)     -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveC(C, J, R, X);
+	checkingMaster(C, J, Mtz)-> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(C,J,QLinha, QColuna,R, 'Cima')
+	,moveC(C, J, R, X);
 	%Baixo
 	B is I-1,
-	checkingMaster(B, J, Mtz, baixo)    -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveB(B, J, R, X);
+	checkingMaster(B, J, Mtz)    -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(B,J,QLinha, QColuna,R, 'Baixo'),
+	moveB(B, J, R, X);
 	%Direita
 	D is J+1,
-	checkingMaster(I, D, Mtz, direita)  -> troca(I, J, Mtz, R), printMatriz(2,3,R), move(I, D, R, X);
-
+	checkingMaster(I, D, Mtz)  -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(I,D,QLinha, QColuna,R, 'Direita'),
+	move(I, D, R, X);
 	!.
+
 moveC(I, J, Mtz, X):-
 	%Cima
 	C is I+1,
-	checkingMaster(C, J, Mtz, cima)     -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveC(C, J, R, X);
+	checkingMaster(C, J, Mtz)     -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(C,J,QLinha, QColuna,R, 'Cima'),
+	moveC(C, J, R, X);
 	%Direita
 	D is J+1,
-	checkingMaster(I, D, Mtz, direita)  -> troca(I, J, Mtz, R), printMatriz(2,3,R), move(I, D, R, X);
+	checkingMaster(I, D, Mtz)  -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(I,D,QLinha, QColuna,R, 'Direita'),
+	move(I, D, R, X);
 	%Esquerda
 	E is J-1,
-	checkingMaster(I, E, Mtz, esquerda) -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveE(I, E, R, X);
+	checkingMaster(I, E, Mtz) -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(I,E,QLinha, QColuna,R, 'Esquerda'),
+	moveE(I, E, R, X);
 	%Baixo
 	B is I-1,
-	checkingMaster(B, J, Mtz, baixo)    -> troca(I, J, Mtz, R), printMatriz(2,3,R), move(B, J, R, X);
-
+	checkingMaster(B, J, Mtz)    -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(B,J,QLinha, QColuna,R, 'Baixo'),
+	move(B, J, R, X);
 	!.
 
 moveB(I, J, Mtz, X):-
 	%Baixo
 	B is I-1,
-	checkingMaster(B, J, Mtz, baixo)    -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveB(B, J, R, X);
+	checkingMaster(B, J, Mtz) -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(B,J,QLinha, QColuna,R, 'Baixo'),
+	moveB(B, J, R, X);
 	%Direita
 	D is J+1,
-	checkingMaster(I, D, Mtz, direita)  -> troca(I, J, Mtz, R), printMatriz(2,3,R), move(I, D, R, X);
+	checkingMaster(I, D, Mtz)  -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(I,D,QLinha, QColuna,R, 'Direita'),
+	move(I, D, R, X);
 	%Esquerda
 	E is J-1,
-	checkingMaster(I, E, Mtz, esquerda) -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveE(I, E, R, X);
+	checkingMaster(I, E, Mtz) -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(I,E,QLinha, QColuna,R, 'Esquerda'),
+	moveE(I, E, R, X);
 	%Cima
 	C is I+1,
-	checkingMaster(C, J, Mtz, cima)     -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveC(C, J, R, X);
-
+	checkingMaster(C, J, Mtz) -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(C,J,QLinha, QColuna,R, 'Cima'),
+	moveC(C, J, R, X);
 	!.
 
 move(I, J, Mtz, X):-
 	%Direita
 	D is J+1,
-	checkingMaster(I, D, Mtz, direita)  -> troca(I, J, Mtz, R), printMatriz(2,3,R), move(I, D, R, X);
+	checkingMaster(I, D, Mtz)  -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(I,D,QLinha, QColuna,R, 'Direita'),
+	move(I, D, R, X);
 	%Cima
 	C is I+1,
-	checkingMaster(C, J, Mtz, cima)     -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveC(C, J, R, X);
+	checkingMaster(C, J, Mtz)     -> troca(I, J, Mtz, R),busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz),mostraDirecao(C,J,QLinha, QColuna,R, 'Cima'),
+	moveC(C, J, R, X);
 	%Baixo
 	B is I-1,
-	checkingMaster(B, J, Mtz, baixo)    -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveB(B, J, R, X);
+	checkingMaster(B, J, Mtz)    -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(B,J,QLinha, QColuna,R, 'Baixo'),
+	moveB(B, J, R, X);
 	%Esquerda
 	E is J-1,
-	checkingMaster(I, E, Mtz, esquerda) -> troca(I, J, Mtz, R), printMatriz(2,3,R), moveE(I, E, R, X);
+	checkingMaster(I, E, Mtz) -> troca(I, J, Mtz, R), busca(I, Mtz, Linha),tamLista(Mtz,QLinha), tamLista(Linha,QColuna),
+	mostraPosicao(I,J),mostraValor(I,J,Mtz),printMatriz(QLinha,QColuna,I,J,Mtz), mostraDirecao(I,E,QLinha, QColuna,R, 'Esquerda'),
+	moveE(I, E, R, X);
 	!.
-
-  movimento(I, J, M, R) :- move(I, J, M, R).
 
 %----------------------------Ideia para movimentação--------------------------------------------
 /*
@@ -194,9 +199,7 @@ verifica se j+1 é < que o tamanho da lista;
 se sim, verifica se a posição (i,j+1) é válida para movimentação;
 Se sim , executa a movimentação(pode ser chamando esse predicado???);
 Se não, proximo movimento (Cima, baixo...).
-
 */
-
 
 % Proximos Passos:
 % 1- Elaborar predicado: Cima, Baixo, Direita, Esquerda.
